4bbdf2d196aa097cdc7ab7059864b6032e626450
Pim van der Heijden <pim@zifzaf.com>
2014-10-20 18:53:02 +0200

    refactor Steve

--- steve.js/lib/steve.js
+++ steve.js/lib/steve.js
@@ -8,9 +8,8 @@ function Steve(skin_image) {
 
     var context = $("<canvas>")[0].getContext("2d");
     context.drawImage(skin_image, 0, 0);
-    var skin = context.getImageData(0, 0, skin_image.width, skin_image.height);
 
-    this.skin = skin;
+    this.skin = context.getImageData(0, 0, skin_image.width, skin_image.height);
     this.skin.subdata = function (left, top, right, bottom) {
         var width = right - left,
             height = bottom - top;
@@ -19,33 +18,52 @@ function Steve(skin_image) {
 
         for (var i = 0; i < height; i++) {
             for (var j = 0; j < width * 4; j++) { // red, green, blue, alpha
-                data[i * width * 4 + j] = skin.data[(i + top) * skin.width * 4 + left * 4 + j];
+                data[i * width * 4 + j] = this.data[(i + top) * this.width * 4 + left * 4 + j];
             }
         }
 
         return data;
     };
 
-    this.torso = this.set_torso(this.skin);
-    this.head = this.set_head(this.skin);
-    this.left_arm = this.set_left_arm(this.skin);
-    this.right_arm = this.set_right_arm(this.skin);
-    this.left_leg = this.set_left_leg(this.skin);
-    this.right_leg = this.set_right_leg(this.skin);
+    this.torso = this.create_torso(this.skin);
+
+    this.head = this.create_head(this.skin);
+    this.head.position.y += 10;
+    this.torso.add(this.head);
+
+    this.left_arm = this.create_left_arm(this.skin);
+    this.left_arm.position.x += 6;
+    this.left_arm.position.y += 4;
+    this.torso.add(this.left_arm);
+
+    this.right_arm = this.create_right_arm(this.skin);
+    this.right_arm.position.x += -6;
+    this.right_arm.position.y += 4;
+    this.torso.add(this.right_arm);
+
+    this.left_leg = this.create_left_leg(this.skin);
+    this.left_leg.position.x += 2;
+    this.left_leg.position.y += -8;
+    this.torso.add(this.left_leg);
+
+    this.right_leg = this.create_right_leg(this.skin);
+    this.right_leg.position.x += -2;
+    this.right_leg.position.y += -8;
+    this.torso.add(this.right_leg);
 
     this.add(this.torso);
 }
 
 Steve.prototype = Object.create(THREE.Object3D.prototype);
 
-Steve.prototype.set_torso = function (skin) {
+Steve.prototype.create_torso = function () {
     var torso_texture = [
-        new THREE.DataTexture(skin.subdata(28, 20, 32, 32), 4, 12),  // left
-        new THREE.DataTexture(skin.subdata(16, 20, 20, 32), 4, 12),  // rigth
-        new THREE.DataTexture(skin.subdata(20, 16, 28, 20), 8, 4),  // top
-        new THREE.DataTexture(skin.subdata(28, 16, 36, 20), 8, 4),  // bottom
-        new THREE.DataTexture(skin.subdata(20, 20, 28, 32), 8, 12),  // front
-        new THREE.DataTexture(skin.subdata(32, 20, 40, 32), 8, 12)   // back
+        new THREE.DataTexture(this.skin.subdata(28, 20, 32, 32), 4, 12),  // left
+        new THREE.DataTexture(this.skin.subdata(16, 20, 20, 32), 4, 12),  // rigth
+        new THREE.DataTexture(this.skin.subdata(20, 16, 28, 20), 8, 4),  // top
+        new THREE.DataTexture(this.skin.subdata(28, 16, 36, 20), 8, 4),  // bottom
+        new THREE.DataTexture(this.skin.subdata(20, 20, 28, 32), 8, 12),  // front
+        new THREE.DataTexture(this.skin.subdata(32, 20, 40, 32), 8, 12)   // back
     ];
 
     var torso_material = torso_texture.map(function (texture) {
@@ -62,14 +80,14 @@ Steve.prototype.set_torso = function (skin) {
     return torso;
 };
 
-Steve.prototype.set_head = function (skin) {
+Steve.prototype.create_head = function () {
     var head_texture = [
-        new THREE.DataTexture(skin.subdata(16, 8, 24, 16), 8, 8),   // left
-        new THREE.DataTexture(skin.subdata(0, 8, 8, 16), 8, 8),   // rigth
-        new THREE.DataTexture(skin.subdata(8, 0, 16, 8), 8, 8),   // top
-        new THREE.DataTexture(skin.subdata(16, 0, 24, 8), 8, 8),   // bottom
-        new THREE.DataTexture(skin.subdata(8, 8, 16, 16), 8, 8),   // front
-        new THREE.DataTexture(skin.subdata(24, 8, 32, 16), 8, 8)    // back
+        new THREE.DataTexture(this.skin.subdata(16, 8, 24, 16), 8, 8),   // left
+        new THREE.DataTexture(this.skin.subdata(0, 8, 8, 16), 8, 8),   // rigth
+        new THREE.DataTexture(this.skin.subdata(8, 0, 16, 8), 8, 8),   // top
+        new THREE.DataTexture(this.skin.subdata(16, 0, 24, 8), 8, 8),   // bottom
+        new THREE.DataTexture(this.skin.subdata(8, 8, 16, 16), 8, 8),   // front
+        new THREE.DataTexture(this.skin.subdata(24, 8, 32, 16), 8, 8)    // back
     ];
 
     var head_material = head_texture.map(function (texture) {
@@ -84,12 +102,12 @@ Steve.prototype.set_head = function (skin) {
         new THREE.MeshFaceMaterial(head_material));
 
     var hat_texture = [
-        new THREE.DataTexture(skin.subdata(48, 8, 56, 16), 8, 8),   // left
-        new THREE.DataTexture(skin.subdata(32, 8, 40, 16), 8, 8),   // rigth
-        new THREE.DataTexture(skin.subdata(40, 0, 48, 8), 8, 8),   // top
-        new THREE.DataTexture(skin.subdata(48, 0, 56, 8), 8, 8),   // bottom
-        new THREE.DataTexture(skin.subdata(40, 8, 48, 16), 8, 8),   // front
-        new THREE.DataTexture(skin.subdata(56, 8, 64, 16), 8, 8)    // back
+        new THREE.DataTexture(this.skin.subdata(48, 8, 56, 16), 8, 8),   // left
+        new THREE.DataTexture(this.skin.subdata(32, 8, 40, 16), 8, 8),   // rigth
+        new THREE.DataTexture(this.skin.subdata(40, 0, 48, 8), 8, 8),   // top
+        new THREE.DataTexture(this.skin.subdata(48, 0, 56, 8), 8, 8),   // bottom
+        new THREE.DataTexture(this.skin.subdata(40, 8, 48, 16), 8, 8),   // front
+        new THREE.DataTexture(this.skin.subdata(56, 8, 64, 16), 8, 8)    // back
     ];
 
     var hat_material = hat_texture.map(function (texture) {
@@ -107,20 +125,17 @@ Steve.prototype.set_head = function (skin) {
     head.add(_head);
     //head.add(_hat);
 
-    head.position.y += 10;
-    this.torso.add(head);
-
     return head;
 };
 
-Steve.prototype.set_left_arm = function (skin) {
+Steve.prototype.create_left_arm = function () {
     var arm_texture = [
-        new THREE.DataTexture(skin.subdata(48, 20, 52, 32), 4, 12),  // left
-        new THREE.DataTexture(skin.subdata(40, 20, 44, 32), 4, 12),  // rigth
-        new THREE.DataTexture(skin.subdata(44, 16, 48, 20), 4, 4),   // top
-        new THREE.DataTexture(skin.subdata(48, 16, 52, 20), 4, 4),   // bottom
-        new THREE.DataTexture(skin.subdata(44, 20, 48, 32), 4, 12),   // front
-        new THREE.DataTexture(skin.subdata(52, 20, 56, 32), 4, 12)    // back
+        new THREE.DataTexture(this.skin.subdata(48, 20, 52, 32), 4, 12),  // left
+        new THREE.DataTexture(this.skin.subdata(40, 20, 44, 32), 4, 12),  // rigth
+        new THREE.DataTexture(this.skin.subdata(44, 16, 48, 20), 4, 4),   // top
+        new THREE.DataTexture(this.skin.subdata(48, 16, 52, 20), 4, 4),   // bottom
+        new THREE.DataTexture(this.skin.subdata(44, 20, 48, 32), 4, 12),   // front
+        new THREE.DataTexture(this.skin.subdata(52, 20, 56, 32), 4, 12)    // back
     ];
 
     var arm_material = arm_texture.map(function (texture) {
@@ -137,21 +152,18 @@ Steve.prototype.set_left_arm = function (skin) {
 
     var arm = new THREE.Object3D();
     arm.add(_arm);
-    arm.position.x += 6;
-    arm.position.y += 4;
-    this.torso.add(arm);
 
     return arm;
 };
 
-Steve.prototype.set_right_arm = function (skin) {
+Steve.prototype.create_right_arm = function () {
     var arm_texture = [
-        new THREE.DataTexture(skin.subdata(48, 20, 52, 32), 4, 12),  // left
-        new THREE.DataTexture(skin.subdata(40, 20, 44, 32), 4, 12),  // rigth
-        new THREE.DataTexture(skin.subdata(44, 16, 48, 20), 4, 4),   // top
-        new THREE.DataTexture(skin.subdata(48, 16, 52, 20), 4, 4),   // bottom
-        new THREE.DataTexture(skin.subdata(44, 20, 48, 32), 4, 12),   // front
-        new THREE.DataTexture(skin.subdata(52, 20, 56, 32), 4, 12)    // back
+        new THREE.DataTexture(this.skin.subdata(48, 20, 52, 32), 4, 12),  // left
+        new THREE.DataTexture(this.skin.subdata(40, 20, 44, 32), 4, 12),  // rigth
+        new THREE.DataTexture(this.skin.subdata(44, 16, 48, 20), 4, 4),   // top
+        new THREE.DataTexture(this.skin.subdata(48, 16, 52, 20), 4, 4),   // bottom
+        new THREE.DataTexture(this.skin.subdata(44, 20, 48, 32), 4, 12),   // front
+        new THREE.DataTexture(this.skin.subdata(52, 20, 56, 32), 4, 12)    // back
     ];
 
     var arm_material = arm_texture.map(function (texture) {
@@ -168,21 +180,18 @@ Steve.prototype.set_right_arm = function (skin) {
 
     var arm = new THREE.Object3D();
     arm.add(_arm);
-    arm.position.x += -6;
-    arm.position.y += 4;
-    this.torso.add(arm);
 
     return arm;
 };
 
-Steve.prototype.set_left_leg = function (skin) {
+Steve.prototype.create_left_leg = function () {
     var leg_texture = [
-        new THREE.DataTexture(skin.subdata(8, 20, 12, 32), 4, 12),  // left
-        new THREE.DataTexture(skin.subdata(0, 20, 4, 32), 4, 12),  // rigth
-        new THREE.DataTexture(skin.subdata(4, 16, 8, 20), 4, 4),   // top
-        new THREE.DataTexture(skin.subdata(8, 16, 12, 20), 4, 4),   // bottom
-        new THREE.DataTexture(skin.subdata(4, 20, 8, 32), 4, 12),   // front
-        new THREE.DataTexture(skin.subdata(12, 20, 16, 32), 4, 12)    // back
+        new THREE.DataTexture(this.skin.subdata(8, 20, 12, 32), 4, 12),  // left
+        new THREE.DataTexture(this.skin.subdata(0, 20, 4, 32), 4, 12),  // rigth
+        new THREE.DataTexture(this.skin.subdata(4, 16, 8, 20), 4, 4),   // top
+        new THREE.DataTexture(this.skin.subdata(8, 16, 12, 20), 4, 4),   // bottom
+        new THREE.DataTexture(this.skin.subdata(4, 20, 8, 32), 4, 12),   // front
+        new THREE.DataTexture(this.skin.subdata(12, 20, 16, 32), 4, 12)    // back
     ];
 
     var leg_material = leg_texture.map(function (texture) {
@@ -200,21 +209,18 @@ Steve.prototype.set_left_leg = function (skin) {
     // left leg
     var leg = new THREE.Object3D();
     leg.add(_leg);
-    leg.position.x += 2;
-    leg.position.y += -8;
-    this.torso.add(leg);
 
     return leg;
 };
 
-Steve.prototype.set_right_leg = function (skin) {
+Steve.prototype.create_right_leg = function () {
     var leg_texture = [
-        new THREE.DataTexture(skin.subdata(8, 20, 12, 32), 4, 12),  // left
-        new THREE.DataTexture(skin.subdata(0, 20, 4, 32), 4, 12),  // rigth
-        new THREE.DataTexture(skin.subdata(4, 16, 8, 20), 4, 4),   // top
-        new THREE.DataTexture(skin.subdata(8, 16, 12, 20), 4, 4),   // bottom
-        new THREE.DataTexture(skin.subdata(4, 20, 8, 32), 4, 12),   // front
-        new THREE.DataTexture(skin.subdata(12, 20, 16, 32), 4, 12)    // back
+        new THREE.DataTexture(this.skin.subdata(8, 20, 12, 32), 4, 12),  // left
+        new THREE.DataTexture(this.skin.subdata(0, 20, 4, 32), 4, 12),  // rigth
+        new THREE.DataTexture(this.skin.subdata(4, 16, 8, 20), 4, 4),   // top
+        new THREE.DataTexture(this.skin.subdata(8, 16, 12, 20), 4, 4),   // bottom
+        new THREE.DataTexture(this.skin.subdata(4, 20, 8, 32), 4, 12),   // front
+        new THREE.DataTexture(this.skin.subdata(12, 20, 16, 32), 4, 12)    // back
     ];
 
     var leg_material = leg_texture.map(function (texture) {
@@ -229,12 +235,9 @@ Steve.prototype.set_right_leg = function (skin) {
         new THREE.MeshFaceMaterial(leg_material));
     _leg.position.y = -4;
 
-    // left leg
+    // right leg
     var leg = new THREE.Object3D();
-    leg.add(_leg.clone());
-    leg.position.x += -2;
-    leg.position.y += -8;
-    this.torso.add(leg);
+    leg.add(_leg);
 
     return leg;
 };
